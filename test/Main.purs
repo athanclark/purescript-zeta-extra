bec2792a7eaf66f0b765443d3b9e9a44858955ae
Athan Clark <athan.clark@gmail.com>
2020-05-01 13:07:32 -0700

    timed component tested

--- test/Main.purs
+++ test/Main.purs
@@ -1,7 +1,7 @@
 module Test.Main where
 
 import Zeta (subscribe, set, get, make)
-import Zeta.Time (every, delay) as Z
+import Zeta.Time (every, delay, since, debounce) as Z
 
 import Prelude
 import Data.Time.Duration (Milliseconds (..))
@@ -28,6 +28,10 @@ timeTests = do
   everyTest
   log " - delay test:"
   delayTest
+  log " - since test:"
+  sinceTest
+  log " - debounce test:"
+  debounceTest
   where
 
     everyTest :: Effect Unit
@@ -40,11 +44,11 @@ timeTests = do
       void $ launchAff do
         -- delay to sidestep race condition
         delay (Milliseconds (0.2 * second))
-        delay (Milliseconds (5.0 * second))
+        delay (Milliseconds (2.0 * second))
         liftEffect do
           clearInterval id
           array <- Ref.read arrayRef
-          if Array.length array == 6
+          if Array.length array == 3
             then log "every test ok!"
             else throw $ "Not 5 in length: " <> show (Array.length array)
 
@@ -67,6 +71,44 @@ timeTests = do
             1 -> throw "old value!"
             2 -> log "delay test ok!"
             _ -> throw $ "unexpected value: " <> show newVal2
+
+
+    sinceTest :: Effect Unit
+    sinceTest = do
+      sig <- make 1
+      newSig <- Z.since (Milliseconds second) sig
+      set 2 sig
+      newVal1 <- get newSig
+      if newVal1
+        then pure unit
+        else throw "new value without delay!"
+      void $ launchAff do
+        delay (Milliseconds second)
+        liftEffect do
+          newVal2 <- get newSig
+          if not newVal2
+            then log "since test ok!"
+            else throw "old value!"
+
+
+    debounceTest :: Effect Unit
+    debounceTest = do
+      sig <- make 1
+      newSig <- Z.debounce (Milliseconds second) sig
+      changesVar <- Ref.new []
+      let recordChanges x = void (Ref.modify (_ <> [x]) changesVar)
+      subscribe recordChanges newSig
+      set 2 sig
+      set 2 sig
+      set 2 sig
+      set 2 sig
+      void $ launchAff do
+        delay (Milliseconds second)
+        liftEffect do
+          changes <- Ref.read changesVar
+          if changes == [1,2]
+            then log "debounce test ok!"
+            else throw $ "More than one change in under debounce time! " <> show (Array.length changes)
 
 
 second :: Number
